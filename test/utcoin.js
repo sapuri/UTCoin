8a1acb56d96a850f8d4bbc0bbdba587eb666de03
Machi <info@minami.xyz>
2017-09-23 23:42:23 +0900

    #2 Fix tests

--- test/utcoin.js
+++ test/utcoin.js
@@ -1,68 +1,58 @@
-var UTCoin = artifacts.require("./UTCoin.sol");
+let UTCoin = artifacts.require("./UTCoin.sol");
 
-contract('UTCoin', function(accounts) {
+contract('UTCoin', accounts => {
 
-  const totalSupply = 10000;
-  it(`should put ${totalSupply} UTCoin in the first account`, function() {
-    return UTCoin.deployed().then(function(instance) {
-      return instance.getBalance.call(accounts[0]);
-    }).then(function(balance) {
+  const totalSupply = 1000000;
+  it(`should put ${totalSupply} UTCoin in the first account`, () => {
+    return UTCoin.deployed().then(instance => {
+      return instance.balanceOf.call(accounts[0]);
+    }).then(balance => {
       assert.equal(balance.valueOf(), totalSupply, `${totalSupply} wasn't in the first account`);
     });
   });
 
-  it("should call a function that depends on a linked library", function() {
-    var meta;
-    var UTCoinBalance;
-    var UTCoinEthBalance;
-
-    return UTCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(accounts[0]);
-    }).then(function(outCoinBalance) {
-      UTCoinBalance = outCoinBalance.toNumber();
-      return meta.getBalanceInEth.call(accounts[0]);
-    }).then(function(outCoinBalanceEth) {
-      UTCoinEthBalance = outCoinBalanceEth.toNumber();
-    }).then(function() {
-      assert.equal(UTCoinEthBalance, 2 * UTCoinBalance, "Library function returned unexpeced function, linkage may be broken");
-    });
-  });
-
-  it("should send coin correctly", function() {
-    var meta;
+  it("should send coin correctly", () => {
+    let utcoin;
 
     // Get initial balances of first and second account.
-    var account_one = accounts[0];
-    var account_two = accounts[1];
+    const account_one = accounts[0];
+    const account_two = accounts[1];
 
-    var account_one_starting_balance;
-    var account_two_starting_balance;
-    var account_one_ending_balance;
-    var account_two_ending_balance;
+    let account_one_starting_balance;
+    let account_two_starting_balance;
+    let account_one_ending_balance;
+    let account_two_ending_balance;
 
-    var amount = 10;
+    const amount = 10;
 
-    return UTCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_starting_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_starting_balance = balance.toNumber();
-      return meta.sendCoin(account_two, amount, {from: account_one});
-    }).then(function() {
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_ending_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_ending_balance = balance.toNumber();
+    return UTCoin.deployed().then(instance => {
+      utcoin = instance;
+      return utcoin.balanceOf.call(account_one);
+    })
+      .then(balance => {
+        account_one_starting_balance = balance.toNumber();
+        return utcoin.balanceOf.call(account_two);
+      })
+      .then(balance => {
+        account_two_starting_balance = balance.toNumber();
+        return utcoin.approve(account_one, amount);
+      })
+      .then(() => {
+        return utcoin.transferFrom(account_one, account_two, amount);
+      })
+      .then(() => {
+        return utcoin.balanceOf.call(account_one);
+      })
+      .then(balance => {
+        account_one_ending_balance = balance.toNumber();
+        return utcoin.balanceOf.call(account_two);
+      })
+      .then(balance => {
+        account_two_ending_balance = balance.toNumber();
 
-      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
-    });
+        assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
+        assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+      });
   });
 
   it("should blacklisted address cannot transaction", () => {
@@ -80,7 +70,7 @@ contract('UTCoin', function(accounts) {
       })
       .then(() => {
         // Send coins from owner_addr to black_addr.
-        return utcoin.sendCoin(black_addr, amount);
+        return utcoin.transfer(black_addr, amount);
       })
       .then(assert.fail)
       .catch(e => {
