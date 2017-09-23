1b2adb0e744a61e92507aea8e35d58ae6710e18f
m1nam1 <info@minami.xyz>
2017-09-24 06:40:03 +0900

    Unlink to ConvertLib

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
 var UTCoin = artifacts.require("./UTCoin.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, UTCoin);
   deployer.deploy(UTCoin);
 };
