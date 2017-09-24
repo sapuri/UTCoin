10440d72dd18c04f6a56c6c41354dd3772f8fb56
m1nam1 <info@minami.xyz>
2017-09-25 05:59:05 +0900

    Add ropsten to truffle.js

--- truffle.js
+++ truffle.js
@@ -7,6 +7,13 @@ module.exports = {
       host: 'localhost',
       port: 8545,
       network_id: '*' // Match any network id
+    },
+    ropsten: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '3',
+      gas: 4000000,
+      from: '0xa8b56F0940b69e87a3b856f6416439AEc022C220'
     }
   }
 }
