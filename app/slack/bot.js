0495b31c6690a8e1c36b451c0601a8896ba03a48
m1nam1 <info@minami.xyz>
2017-10-29 21:14:03 +0900

    [Slackbot] Check balance of `from_address`

--- app/slack/bot.js
+++ app/slack/bot.js
@@ -160,6 +160,7 @@ controller.on(['reaction_added'], (bot, message) => {
           const deposit_balance_fixed = value.div(num_suffix).toFixed(decimals); // string
           bot.botkit.log(`Deposit balance: ${deposit_balance_fixed} UTC`);
           if (deposit_balance < tip_amount) {
+            bot.botkit.log.error('the deposit balance is insufficient!');
             bot.reply(message, 'The deposit balance is insufficient!');
           } else {
             // Send UTCoin from `deposit_address` to `to_address`
@@ -205,44 +206,60 @@ controller.hears('send ([0-9]+[\.]?[0-9]*) UTC to (.+)', 'direct_message', (bot,
     } else {
       from_address = user.address;
 
-      // Get `to_address`
-      controller.storage.users.get(to_user, (err, user) => {
-        if (!user) {
-          bot.botkit.log.error('target address not found');
-          bot.reply(message, 'I cannot find target address. It seems that it is not registered yet.');
-        } else {
-          to_address = user.address;
-
-          // Transaction
-          bot.startConversation(message, (err, convo) => {
-            convo.ask(`OK. I will send *${amount / num_suffix} UTC* to <@${to_user}>.\nIf it is correct, please enter your password. (Your address: \`${from_address}\`)`, (response, convo) => {
-              const password = response.text;
-              if (web3.personal.unlockAccount(from_address, password)) {
-                // Send UTCoin from `from_address` to `to_address`
-                bot.botkit.log(`Transfer ${amount / num_suffix} UTC from ${from_address} to ${to_address}.`);
-                bot.reply(message, 'Please wait...');
-                UTCoin.at(utcoin_address)
-                  .then(instance => {
-                    return instance.transfer(to_address, amount, {from: from_address});
-                  })
-                  .then(() => {
-                    bot.botkit.log('Transaction complete!');
-                    convo.say('The transaction has been completed!');
-                    convo.next();
-                  })
-                  .catch(e => {
-                    bot.botkit.log.error(e);
-                    convo.say('The transaction failed. Please contact the administrator.');
-                    convo.next();
-                  });
+      // Check balance of `from_address`
+      UTCoin.at(utcoin_address).then(instance => {
+        return instance.balanceOf.call(from_address);
+      })
+        .then(value => {
+          const balance = value.toNumber();
+          if (value < amount) {
+            bot.botkit.log.error('insufficient funds');
+            bot.reply(message, 'Your balance is insufficient.');
+          } else {
+            // Get `to_address`
+            controller.storage.users.get(to_user, (err, user) => {
+              if (!user) {
+                bot.botkit.log.error('target address not found');
+                bot.reply(message, 'I cannot find target address. It seems that it is not registered yet.');
               } else {
-                convo.say('Sorry. This password is incorrect.');
-                convo.next();
+                to_address = user.address;
+
+                // Transaction
+                bot.startConversation(message, (err, convo) => {
+                  convo.ask(`OK. I will send *${amount / num_suffix} UTC* to <@${to_user}>.\nIf it is correct, please enter your password. (Your address: \`${from_address}\`)`, (response, convo) => {
+                    const password = response.text;
+                    if (web3.personal.unlockAccount(from_address, password)) {
+                      // Send UTCoin from `from_address` to `to_address`
+                      bot.botkit.log(`Transfer ${amount / num_suffix} UTC from ${from_address} to ${to_address}.`);
+                      bot.reply(message, 'Please wait...');
+                      UTCoin.at(utcoin_address)
+                        .then(instance => {
+                          return instance.transfer(to_address, amount, {from: from_address});
+                        })
+                        .then(() => {
+                          bot.botkit.log('Transaction complete!');
+                          convo.say('The transaction has been completed!');
+                          convo.next();
+                        })
+                        .catch(e => {
+                          bot.botkit.log.error(e);
+                          convo.say('The transaction failed. Please contact the administrator.');
+                          convo.next();
+                        });
+                    } else {
+                      bot.botkit.log.error('invalid password');
+                      convo.say('Sorry. This password is incorrect.');
+                      convo.next();
+                    }
+                  });
+                });
               }
             });
-          });
-        }
-      });
+          }
+        })
+        .catch(e => {
+          bot.botkit.log.error(e);
+        });
     }
   });
 });
